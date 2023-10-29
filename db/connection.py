98ce70df2f4d09ab4eb05175602773c1c17264a8
Emilian Ognianov <emilian.e.ognianov@gmail.com>
2023-10-29 18:05:23 +0200

    KH-29/setup sql alchemy and alembic (#5)
    * -feat- base sqlalchemy
    
    * -feat- alembic setup

---
+++ db/connection.py
@@ -0,0 +1,46 @@
+"""DB connection module"""
+import sqlalchemy
+import sqlalchemy.orm
+import configuration
+
+config = configuration.Config()
+
+CONNECTION_STRING = config.connection_string
+
+
+def get_engine() -> sqlalchemy.Engine:
+    """
+    Return engine
+    :return:
+    """
+
+    echo = config.context == configuration.ContextOptions.DEV
+    if config.database == configuration.DbTypeOptions.SQLITE and config.sqlite.is_in_memory:
+        return sqlalchemy.create_engine(CONNECTION_STRING, echo=echo, connect_args={"check_same_thread": False},
+                                        poolclass=sqlalchemy.StaticPool)
+    return sqlalchemy.create_engine(CONNECTION_STRING, echo=echo)
+
+
+def get_connection(engine: sqlalchemy.Engine = None) -> sqlalchemy.Connection:
+    """
+    Get connection
+
+    :param engine:
+    :return:
+    """
+
+    if not engine:
+        engine = get_engine()
+    return engine.connect()
+
+
+def get_session(engine: sqlalchemy.Engine = None) -> sqlalchemy.orm.Session:
+    """
+    Get session
+    :param engine:
+    :return:
+    """
+
+    if not engine:
+        engine = get_engine()
+    return sqlalchemy.orm.Session(bind=engine, autocommit=False, autoflush=False)
---
+++ db/models.py
@@ -0,0 +1,7 @@
+"""DB Models module"""
+from sqlalchemy.orm import DeclarativeBase
+
+
+class DbBaseModel(DeclarativeBase):
+    """DB base model"""
+    ...
