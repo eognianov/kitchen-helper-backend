a324912c0f0e7347dd57c65d3171811bf238c06e
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-11-06 10:00:16 +0200

    -feat- changed create user input model

--- features/users/input_models.py
+++ features/users/input_models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, constr, EmailStr
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 import re
 import configuration
@@ -7,39 +7,27 @@ config = configuration.Config()
 
 
 class RegisterUserInputModel(BaseModel):
-    id: int
-    username: constr(min_length=3, max_length=30)
-    email: EmailStr  # EmailStr for email validation
-    password: constr()
+    username: str = Field(min_length=3, max_length=30)
+    email: str = EmailStr  # for email validation
+    password: str = Field()
 
-    @staticmethod
-    def validate_password_dev(password):
-        # Password validation logic for 'dev' context
-        if len(password) < 4:
-            raise ValueError("Password must be at least 4 characters long in 'dev' context")
-
-    @staticmethod
-    def validate_password_prod(password):
-        # Password validation logic for 'prod' context
-        if len(password) < 8:
-            raise ValueError("Password must be at least 8 characters long")
-        if not re.search(r'[A-Z]', password):
-            raise ValueError("Password must contain at least one uppercase letter")
-        if not re.search(r'[a-z]', password):
-            raise ValueError("Password must contain at least one lowercase letter")
-        if not re.search(r'[0-9]', password):
-            raise ValueError("Password must contain at least one digit")
-        if not re.search(r'[!@#$%^&*?]', password):
-            raise ValueError("Password must contain at least one special symbol: !@#$%^&*?")
-        return password
-
-    @staticmethod
-    def validate_password(password, context=config.context):
-        if context == 'dev':
-            RegisterUserInputModel.validate_password_dev(password)
+    @field_validator('password', mode='after')
+    @classmethod
+    def validate_password(cls, password):
+        if config.context == configuration.ContextOptions.DEV:
+            if len(password) < 4:
+                raise ValueError("Password must be at least 4 characters long in 'dev' context")
         else:
-            RegisterUserInputModel.validate_password_prod(password)
-        return password
+            if len(password) < 8:
+                raise ValueError("Password must be at least 8 characters long")
+            if not re.search(r'[A-Z]', password):
+                raise ValueError("Password must contain at least one uppercase letter")
+            if not re.search(r'[a-z]', password):
+                raise ValueError("Password must contain at least one lowercase letter")
+            if not re.search(r'[0-9]', password):
+                raise ValueError("Password must contain at least one digit")
+            if not re.search(r'[!@#$%^&?]', password):
+                raise ValueError("Password must contain at least one special symbol: !@#$%^&?")
 
 
 class UsersResponseModel(BaseModel):
