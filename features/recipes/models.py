4173410c5c965ae43117da74ccc94e4c9bb46a2a
Emiliyan Ognyanov <emilian.e.ognianov@gmail.com>
2023-11-07 20:52:00 +0200

    -feat- fix data type

--- features/recipes/models.py
+++ features/recipes/models.py
@@ -1,6 +1,6 @@
 from features import DbBaseModel
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, Integer, Float, DateTime, func, DATETIME, ForeignKey
+from sqlalchemy import String, Integer, Float, func, ForeignKey, DateTime
 import datetime
 from typing import Optional
 
@@ -12,9 +12,9 @@ class RecipeCategory(DbBaseModel):
     id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True, init=False)
     name: Mapped[str] = mapped_column(String(50), unique=True)
     created_by: Mapped[str] = mapped_column(String(30))
-    created_on: Mapped[datetime.datetime] = mapped_column(DATETIME, server_default=func.current_timestamp(), init=False)
+    created_on: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.current_timestamp(), init=False)
     updated_by: Mapped[Optional[str]] = mapped_column(String(30), nullable=True, init=False)
-    updated_on: Mapped[datetime.datetime] = mapped_column(DATETIME, server_default=func.current_timestamp(), onupdate=func.current_timestamp(), init=False)
+    updated_on: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.current_timestamp(), onupdate=func.current_timestamp(), init=False)
     recipes: Mapped[list["Recipe"]] = relationship("Recipe", back_populates="category", init=False)
 
 
@@ -33,8 +33,8 @@ class Recipe(DbBaseModel):
     cholesterol: Mapped[Optional[float]] = mapped_column(Float, nullable=True, init=False)
     time_to_prepare: Mapped[int] = mapped_column(Integer)
     created_by: Mapped[str] = mapped_column(String(30))
-    created_on: Mapped[datetime.datetime] = mapped_column(DATETIME, server_default=func.current_timestamp(), init=False)
+    created_on: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.current_timestamp(), init=False)
     updated_by: Mapped[Optional[str]] = mapped_column(String(30), nullable=True, init=False)
-    updated_on: Mapped[datetime.datetime] = mapped_column(DATETIME, server_default=func.current_timestamp(), onupdate=func.current_timestamp(), init=False)
+    updated_on: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.current_timestamp(), onupdate=func.current_timestamp(), init=False)
     category_id: Mapped[int] = mapped_column(ForeignKey("RECIPE_CATEGORIES.id"), nullable=True, init=False)
     category: Mapped[RecipeCategory] = relationship("RecipeCategory", back_populates="recipes", init=False)
