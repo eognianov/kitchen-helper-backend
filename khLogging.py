628c0df6e4732ea12f2d95116651f6c881df966e
Emiliyan Ognyanov <emilian.e.ognianov@gmail.com>
2023-11-09 16:23:24 +0200

    -refactor- optimize logger class

--- khLogging.py
+++ khLogging.py
@@ -2,15 +2,16 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 import configuration
+from collections import defaultdict
 
 
 class Logger:
-    child_logger_registration = {}
-    loggers = []
+    child_logger_registration = defaultdict(list)
+    logger = None
 
-    def __init__(self, log_name: str, child_loggers=None):
+    def __init__(self, log_name: str, child_logger=False):
 
-        if child_loggers is None:
+        if not child_logger:
             self.logger = logging.getLogger(log_name)
             self.logger.setLevel(logging.DEBUG)
 
@@ -25,58 +26,42 @@ class Logger:
             )
             log_handler.setFormatter(log_formatter)
             self.logger.addHandler(log_handler)
-            self.child_loggers = None
-            Logger.loggers.append(self.logger)
+            self.child_logger = False
+            Logger.logger = self.logger
         else:
             self.name = log_name
-            self.child_loggers = child_loggers
-
-    def is_child_logger(self):
-        return bool(self.child_loggers)
+            self.child_logger = child_logger
 
     def _check_for_unregistered_loggers(self):
-        for parent_logger in Logger.loggers:
-            if parent_logger not in Logger.child_logger_registration[self.logger.name]:
-                child_logger = logging.getLogger(f"{parent_logger.name}.{self.logger.name}")
-                self.child_loggers.append(child_logger)
-                Logger.child_logger_registration[self.logger.name].append(parent_logger)
+        parent_logger = Logger.logger
+        if parent_logger and parent_logger not in Logger.child_logger_registration[self.name]:
+            self.child_logger = logging.getLogger(f"{parent_logger.name}.{self.name}")
+            Logger.child_logger_registration[self.name].append(parent_logger)
 
-    def log(self, level, *args, **kwargs):
-        if self.is_child_logger():
-            self._log_to_child(level, *args)
+    def _log(self, level, *args, **kwargs):
+        if self.child_logger:
+            self._check_for_unregistered_loggers()
+            getattr(self.child_logger, level)(*args)
         else:
-            self._log_to_parent(level, *args)
-
-    def _log_to_child(self, level, *args):
-        self._check_for_unregistered_loggers()
-        for logger in self.child_loggers:
-            getattr(logger, level)(*args)
-
-    @staticmethod
-    def _log_to_parent(level, *args):
-        for parent_logger in Logger.loggers:
-            getattr(parent_logger, level)(*args)
+            if not Logger.logger:
+                Logger('undefined.log')
+            getattr(Logger.logger, level)(*args)
 
     def debug(self, *args):
-        self.log('debug', *args)
+        self._log('debug', *args)
 
     def info(self, *args):
-        self.log('info', *args)
+        self._log('info', *args)
 
     def warning(self, *args):
-        self.log('warning', *args)
+        self._log('warning', *args)
 
     def error(self, *args):
-        self.log('error', *args)
+        self._log('error', *args)
 
     def exception(self, *args, **kwargs):
-        self.log('error', *args, **kwargs)
+        self._log('error', *args, **kwargs)
 
     @classmethod
     def get_child_logger(cls, log_name: str):
-        child_loggers = []
-        for parent_logger in Logger.loggers:
-            child_logger = logging.getLogger(f"{parent_logger.name}.{log_name}")
-            child_loggers.append(child_logger)
-            Logger.child_logger_registration.setdefault(log_name, []).append(parent_logger)
-        return cls(log_name, child_loggers=child_loggers)
+        return cls(log_name, child_logger=True)
