8c5d37e252d78dea463cf0a4d520a16c4bd2a492
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-11-22 15:48:48 +0200

    added EmailConfirmationToken model and is_email_confirmed filed to User model

--- features/users/models.py
+++ features/users/models.py
@@ -1,7 +1,9 @@
-from sqlalchemy import String, LargeBinary
+from sqlalchemy import String, LargeBinary, ForeignKey, Boolean, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from features import DbBaseModel
+
+import datetime
 
 
 class User(DbBaseModel):
@@ -11,3 +13,14 @@ class User(DbBaseModel):
     username: Mapped[str] = mapped_column(String(30), unique=True)
     email: Mapped[str] = mapped_column(String(50), unique=True)
     password: Mapped[bytes] = mapped_column(LargeBinary())
+    is_email_confirmed: Mapped[bool] = mapped_column(Boolean, default=False)
+
+
+class EmailConfirmationToken(DbBaseModel):
+    __tablename__ = 'TOKEN'
+
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    email_confirmation_token: Mapped[str] = mapped_column(String(43), unique=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("Users.id"))
+    created_on: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.current_timestamp(), init=False)
+    expired_on: Mapped[datetime.datetime] = mapped_column(DateTime)
