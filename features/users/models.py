59099ff0a39a9f832e608ecb7ac6d8a176bd179c
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-11-22 15:48:48 +0200

    added EmailConfirmationToken model and is_email_confirmed filed to User model

--- features/users/models.py
+++ features/users/models.py
@@ -3,8 +3,12 @@ from typing import Optional
 
 from sqlalchemy import String, LargeBinary, Column, Table, ForeignKey, DateTime, func, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, LargeBinary, ForeignKey, Boolean, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
 
 from features import DbBaseModel
+
+import datetime
 
 
 class User(DbBaseModel):
@@ -14,6 +18,17 @@ class User(DbBaseModel):
     username: Mapped[str] = mapped_column(String(30), unique=True)
     email: Mapped[str] = mapped_column(String(50), unique=True)
     password: Mapped[bytes] = mapped_column(LargeBinary())
+    is_email_confirmed: Mapped[bool] = mapped_column(Boolean, default=False)
+
+
+class EmailConfirmationToken(DbBaseModel):
+    __tablename__ = 'TOKEN'
+
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    email_confirmation_token: Mapped[str] = mapped_column(String(43), unique=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("Users.id"))
+    created_on: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.current_timestamp(), init=False)
+    expired_on: Mapped[datetime.datetime] = mapped_column(DateTime)
 
     roles = relationship('Role', secondary='user_roles', back_populates='users', lazy='selectin')
 
