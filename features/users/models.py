39c1f5069fd77f4e80dc29189fbf5dad2b54316e
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-11-22 12:34:25 +0200

    added init False to id field of user model

--- features/users/models.py
+++ features/users/models.py
@@ -7,7 +7,7 @@ from features import DbBaseModel
 class User(DbBaseModel):
     __tablename__ = 'Users'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
     username: Mapped[str] = mapped_column(String(30), unique=True)
     email: Mapped[str] = mapped_column(String(50), unique=True)
     password: Mapped[bytes] = mapped_column(LargeBinary())
