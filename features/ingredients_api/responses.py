337962a30ce3047d640a89228b244eec9034d07c
Javor Mirchev <103359449+Javor18@users.noreply.github.com>
2023-11-23 03:05:54 +0200

    Adding some fixtures from the comments from ingredients branch

--- features/ingredients_api/responses.py
+++ features/ingredients_api/responses.py
@@ -11,16 +11,3 @@ class IngredientCategory(pydantic.BaseModel):
         updated_by: str = pydantic.Field(max_length=30)
         updated_on: str = pydantic.Field()
         ingredients: list["Ingredient"] = pydantic.Field(default=list)
-
-
-class Ingredient(pydantic.BaseModel):
-
-    id: int = pydantic.Field(autoincrement=True, primary_key= True)
-    name: str = pydantic.Field(max_length=255)
-    category: str = pydantic.Field(max_length=200)
-    calories: float = pydantic.Field(default=0, ge=0)
-    carbo: float = pydantic.Field(default=0, ge=0)
-    fats: float = pydantic.Field(default=0, ge=0)
-    protein: float = pydantic.Field(default=0, ge=0)
-    cholesterol: float = pydantic.Field(default=0, ge=0)
-    measurement: float = pydantic.Field(default=0, ge=0)
