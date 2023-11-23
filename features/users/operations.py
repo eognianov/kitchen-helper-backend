bfad5d4fbbc535452183d0fed429560884e2e4a0
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-11-23 10:18:29 +0200

    reset password functionality

--- features/users/operations.py
+++ features/users/operations.py
@@ -247,8 +247,8 @@ def remove_user_from_role(user_id: int, role_id: int) -> None:
         session.commit()
 
 
-async def send_email(*, user: User, subject: str, content: str, recipient: str):
-    # TODO: setup from_email and host in html_content
+async def send_email(*, subject: str, content: str, recipient: str):
+    # TODO: setup from_email
     message = Mail(
         from_email='your@example.com',
         to_emails=recipient,
@@ -286,6 +286,8 @@ def generate_email_confirmation_token(user: User, expiration_days: int = 7):
 
 
 def get_token_from_db(token: str, token_type: str):
+    # Get the token from db if exists, check if token is expired and delete it
+
     model = EmailConfirmationToken if token_type == 'email' else PasswordResetToken
     current_datetime = datetime.utcnow()
 
@@ -306,20 +308,24 @@ def get_token_from_db(token: str, token_type: str):
 
 
 def confirm_email(user_id: int) -> User:
+    # Mark the user email as confirmed and delete the token
+
     user = get_user_from_db(pk=user_id)
-    # Mark the user email as confirmed
     user.is_email_confirmed = True
+
     with get_session() as session:
-        # Fetch the token object
         token = session.query(
             EmailConfirmationToken
         ).filter(EmailConfirmationToken.user_id == user.id).first()
         session.delete(token)
         session.commit()
+
     return user
 
 
-def generate_password_reset_token(user: User, expiration_hours: int = 1):
+def generate_password_reset_token(user: User, expiration_hours: int = 1) -> str:
+    # Generate password reset token and set expiration time
+
     token = secrets.token_urlsafe(32)
     expiration_time = datetime.utcnow() + timedelta(hours=expiration_hours)
 
@@ -335,6 +341,18 @@ def generate_password_reset_token(user: User, expiration_hours: int = 1):
     return token
 
 
-def update_user_password(user, new_password):
+def update_user_password(user: User, new_password: str) -> User:
+    # Hash the new password, change it for the requested user and delete the token
+
     hashed_password = hash_password(new_password)
     user.password = hashed_password
+
+    with get_session() as session:
+        # Fetch the token object
+        token = session.query(
+            PasswordResetToken
+        ).filter(PasswordResetToken.user_id == user.id).first()
+        session.delete(token)
+        session.commit()
+
+    return user
