fec391c3aedac32f9d4c5dafc0de7a267242f137
Javor Mirchev <103359449+Javor18@users.noreply.github.com>
2023-12-07 21:19:30 +0200

    Ingredients

--- features/recipes/ingredients/input_models.py
+++ features/recipes/ingredients/input_models.py
@@ -41,17 +41,12 @@ class CreateIngredientInputModel(BaseModel):
 class UpgradeIngredientInputModel(BaseModel):
     """Update ingredient"""
 
-    name: Optional[str] = Field(None, max_length=255)
-    category: Optional[str] = Field(None, max_length=255)
-    calories: Optional[float] = Field(None, ge=0)
-    carbo: Optional[float] = Field(None, ge=0)
-    fats: Optional[float] = Field(None, ge=0)
-    proteins: Optional[float] = Field(None, ge=0)
-    cholesterol: Optional[float] = Field(None, ge=0)
-    measurement: Optional[float] = Field(None, ge=0)
+    name: str = Field(min_length=3, max_length=255)
+    category: str = Field(max_length=255)
+    calories: float = Field(default=0, ge=0)
+    carbo: float = Field(default=0, ge=0)
+    fats: float = Field(default=0, ge=0)
+    proteins: float = Field(default=0, ge=0)
+    cholesterol: float = Field(default=0, ge=0)
+    measurement: float = Field(default=0, ge=0)
 
-    @validator("name")
-    def validate_name(cls, name):
-        if name is not None and len(name) < 3:
-            raise ValueError("Name must be at least 3 characters long")
-        return name
