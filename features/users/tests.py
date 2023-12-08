0419342792e50303da6411ae15c907aac38f59c1
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-12-08 18:56:59 +0200

    added test for signin user

--- features/users/tests.py
+++ features/users/tests.py
@@ -6,21 +6,29 @@ from features.users import operations
 from features.users.models import User
 from fastapi.testclient import TestClient
 from api import app
+import configuration
 
 
 class TestUserOperations:
     client = TestClient(app)
+    user_data = {
+        'username': 'test_user',
+        'email': '_test_example@abv.bg',
+        'password': 'secure_password!2'
+    }
 
     def test_create_new_user_success(self, use_test_db, mocker):
-        user_data = {
-            'username': 'test_user',
-            'email': '_test_example@abv.bg',
-            'password': 'secure_password!2'
-        }
 
-        operations.create_new_user(RegisterUserInputModel(**user_data))
+        operations.create_new_user(RegisterUserInputModel(**self.user_data))
         with db.connection.get_session() as session:
             users = session.query(User).all()
-        assert users[0].username == user_data['username']
-        assert users[0].email == user_data['email']
+        assert users[0].username == self.user_data['username']
+        assert users[0].email == self.user_data['email']
         assert users[0].id == 1
+
+    def test_signin_user_success(self, use_test_db, mocker):
+        operations.create_new_user(RegisterUserInputModel(**self.user_data))
+        config = configuration.Config()
+        print(config.jwt.algorithm)
+        token, token_type = operations.signin_user(self.user_data['username'], self.user_data['password'])
+        assert token_type == 'jwt access token'
