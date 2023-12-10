6a071b0cddf516583b84d5cfd3a4be2e9892430f
Javor Mirchev <103359449+Javor18@users.noreply.github.com>
2023-12-10 12:46:24 +0200

    Ingredients functionality

--- features/__init__.py
+++ features/__init__.py
@@ -3,4 +3,3 @@ from db.models import DbBaseModel
 from features.users.models import *
 from features.recipes.models import *
 from features.images.models import *
-from features.recipes.ingredients.models import *
