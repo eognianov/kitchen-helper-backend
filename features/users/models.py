1b14d79ffcba7bd55e0f3022f10b30e35a37e552
Emiliyan Ognyanov <emilian.e.ognianov@gmail.com>
2023-12-11 19:06:47 +0200

    Merge branch 'master' of https://github.com/eognianov/kitchen-helper-backend into KH-24/Add-authorization
    # Conflicts:
    #	features/users/models.py
    #	features/users/operations.py
    #	features/users/responses.py
    #	features/users/router.py

--- features/users/models.py
+++ features/users/models.py
@@ -15,7 +15,7 @@ class User(DbBaseModel):
     username: Mapped[str] = mapped_column(String(30), unique=True)
     email: Mapped[str] = mapped_column(String(50), unique=True)
     password: Mapped[bytes] = mapped_column(LargeBinary())
-    is_email_confirmed: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_email_confirmed: Mapped[bool] = mapped_column(Boolean, default=False, server_default='false')
     # TODO add updated by and update on
 
     roles = relationship('Role', secondary='user_roles', back_populates='users', lazy='selectin')
