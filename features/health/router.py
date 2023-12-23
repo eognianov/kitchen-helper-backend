b1366efa35fa32b6d9a7fb3d92d038056af35e73
Martin Deyanov <justdeyanov@gmail.com>
2023-12-23 19:16:52 +0200

    extract function to operation file

--- features/health/router.py
+++ features/health/router.py
@@ -1,24 +1,8 @@
 import fastapi
-from sqlalchemy import exc as sqlalchemy_exc
-
-from db.connection import get_engine, get_connection
+from .operations import check_db_status
 from . import responses
 
 router = fastapi.APIRouter()
-
-
-def check_db_status() -> str:
-    try:
-        engine = get_engine()
-        with get_connection(engine) as connection:
-            connection.execute('SELECT 1')
-        return "Working"
-    except sqlalchemy_exc.DBAPIError as e:
-        print(f"Database error: {e}")
-        return "Not Working"
-    except Exception as e:
-        print(f"Unexpected error: {e}")
-        return "Not Working"
 
 
 @router.get('/', response_model=responses.BaseHealthResponse)
