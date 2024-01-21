fe47029f2cc08e93bf14473dab1e5c3ffbe1b3dc
Emiliyan Ognyanov <emilian.e.ognianov@gmail.com>
2024-01-21 20:26:54 +0200

    -feat- add call to the grpc server

--- features/images/grpc_server.py
+++ features/images/grpc_server.py
@@ -5,11 +5,17 @@ import communication.images_pb2
 import communication.images_pb2_grpc
 import configuration
 import features.images.operations
+import khLogging
 
 
 class ImagesServicer(communication.images_pb2_grpc.ImagesServicer):
     def get_image_url(self, request, context):
-        message = communication.images_pb2.ImageResponse(image_url='url')
+        try:
+            image = features.images.operations.get_image(request.image_id)
+            image_url = features.images.operations.generate_image_url(image.name, image.in_cloudinary)
+        except:
+            image_url = None
+        message = communication.images_pb2.ImageResponse(image_url=image_url)
         return message
 
 
--- features/images/responses.py
+++ features/images/responses.py
@@ -1,9 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
-
-import configuration
-from .constants import IMAGES_DIR
-from pathlib import Path
+import features.images.operations
 
 
 class ImageResponse(BaseModel):
@@ -17,7 +14,5 @@ class ImageResponse(BaseModel):
     url: str = ""
 
     def model_post_init(self, __context):
-        if not self.in_cloudinary:
-            self.url = f"{Path.joinpath(IMAGES_DIR, self.name).relative_to(configuration.ROOT_PATH)}"
-        else:
-            self.url = f"https://res.cloudinary.com/{configuration.Cloudinary().cloud_name}/image/upload/{self.name}"
+        _url = features.images.operations.generate_image_url(self.name, self.in_cloudinary)
+        self.url = _url
